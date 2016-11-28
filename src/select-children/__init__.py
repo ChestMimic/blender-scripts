9503572c2624714f97468af4d01fc39dbfb9d00b
Fitzgibbon <ibbolia@gmail.com>
2016-11-28 15:54:13 -0500

    Added comments, updated copyright year

--- src/select-children/__init__.py
+++ src/select-children/__init__.py
@@ -1,6 +1,6 @@
 # BEGIN GPL BLOCK    
 #    Blender Renderwatch monitors the status of the Render operation
-#    Copyright (C) 2015  Mark Fitzgibbon
+#    Copyright (C) 2016  Mark Fitzgibbon
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -24,34 +24,33 @@ bl_info = {
 import bpy
 
 def getChildren(target):
-    res = []
-    scene = bpy.data.objects
-    for OBJ in scene:
-        if OBJ.parent == target:
-            res.append(OBJ)
-            OBJ.select = True
-    return res
+    res = []                                #Initialize empty list
+    scene = bpy.data.objects                #Assign scene, most useful root position
+    for OBJ in scene:                       #Run check against all object in scene
+        if OBJ.parent == target:            #An object's parent data matches given target
+            res.append(OBJ)                 #Add to list
+            OBJ.select = True               #Select object in Blender UI
+    return res                              #Return objects
 
 def selectChildrenR(target):
-    if target == []:
-        return []
+    if target == []:                        #Nothing to work with
+        return []                           #Return empty list
     else:
-        for PARENT in target:
-            childLst = getChildren(PARENT)
-            selectChildrenR(childLst)
+        for PARENT in target:               #Each possible parent in list
+            childLst = getChildren(PARENT)  #Get the object's children as a list
+            selectChildrenR(childLst)       #Call function on all of object's children
 
 class SelectChildren(bpy.types.Operator):
-    bl_idname = "object.select_children"
-    bl_label = "Select All Children"
-    bl_options = {"REGISTER", "UNDO"}
+    bl_idname = "object.select_children"    #id name
+    bl_label = "Select All Children"        #Display Label
+    bl_options = {"REGISTER", "UNDO"}       #Possible operations
     
     def execute(self, context):
-        lst = bpy.context.selected_objects
-        childlist = getChildren(lst[0])
+        lst = bpy.context.selected_objects  #List of all selected objects
+        #TODO: Next 2 lines look off...
+        childlist = getChildren(lst[0])     
         selectChildrenR(lst)
         return {'FINISHED'}
-
-    
 
 def register():
     bpy.utils.register_class(SelectChildren)
