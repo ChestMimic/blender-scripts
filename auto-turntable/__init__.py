acd296ee3bb25d7c233f6043bb59ff5ea34ed3fd
Fitzgibbon <ibbolia@gmail.com>
2016-12-12 09:49:19 -0500

    Added toFrontView method

--- auto-turntable/__init__.py
+++ auto-turntable/__init__.py
@@ -1,10 +1,21 @@
 import bpy
-from bpy import context
 from math import radians
 
-obj_cam = bpy.data.objects["Camera"]
+TT_ANGLE_INCREMENTS = 90
 
-obj_cam.location = (0.0, -5.0, 0.0)
-obj_cam.rotation_euler = (radians(90), 0.0, 0.0)
-bpy.ops.view3d.camera_to_view_selected()
-bpy.ops.render.render(animation=False)
+def setToFrontview(camera):
+	#Move camera front and center
+	camera.location = (0.0, -5.0, 0.0)
+	#Orient Camera pointing to Origin (0,0,0)
+	camera.rotation_euler = (radians(90), 0.0, 0.0)
+	#reposition camera to fit selected items
+	bpy.ops.view3d.camera_to_view_selected()
+
+def renderOrbit(camera):
+	#Take render
+	#bpy.ops.render.render(animation=False)
+	pass
+
+if __name__ == "__main__":
+	obj_cam = bpy.data.objects["Camera"]
+	setToFrontview(obj_cam)
