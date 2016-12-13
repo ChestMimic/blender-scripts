2b979743af0662716e399514d702fe057e477519
Fitzgibbon <ibbolia@gmail.com>
2016-12-13 10:28:42 -0500

    Added license block, coments to auto-turntable. renamed bl-info to match folder structure

--- auto-turntable/__init__.py
+++ auto-turntable/__init__.py
@@ -1,3 +1,21 @@
+# BEGIN GPL BLOCK    
+#    Blender Renderwatch monitors the status of the Render operation
+#    Copyright (C) 2016  Mark Fitzgibbon
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# END GPL BLOCK
 bl_info = {
 	"name":"Automatic Turntable",
 	"description":"Automatically focus camera to rotate around a selected object in a scene",
@@ -13,7 +31,8 @@ import mathutils
 from math import radians
 
 class Orbital:
-
+	'''Orbital class contains variables and functions for positioning and rotating camera around a model
+		'''
 	def __init__(self):
 		self.TT_FILEPATH_ROOT = "/tmp\\"
 		self.TT_FILEPATH_ITERATOR = "1"
@@ -26,7 +45,11 @@ class Orbital:
 		self.TT_ROTATION_Z = 0.0
 		self.TT_RADIUS = 0.0
 
+
 	def setToFrontview(self, camera):
+		'''sets camera of choice to a directly front view position (assuming "Front" is negative Y)
+			camera -- Camera object to be positioned
+			'''
 		#Move camera front and center
 		camera.location = self.TT_POSITION
 		camera.rotation_euler = (radians(self.TT_ROTATION_X), 0.0, 0.0)
@@ -36,6 +59,9 @@ class Orbital:
 		self.TT_RADIUS = camera.location[1]	#currently, Y coordinate == radius
 
 	def renderOrbit(self, camera):
+		'''Iterate tghrough radial steps and render at position
+			camera -- Camera object to perform on
+			'''
 		while(self.TT_ROTATION_Z < 270):
 			#Take render
 			print("Camera at (" +str(self.TT_POSITION[0]) +"," + str(self.TT_POSITION[1]) +"," + str(self.TT_POSITION[2])  +")" )
@@ -52,7 +78,8 @@ class Orbital:
 			#bpy.ops.mesh.primitive_cube_add(location=self.TT_POSITION) 
 
 class OrbitalOperator(bpy.types.Operator):
-	
+	'''Class to interface with blender python
+		'''	
 	bl_idname = "object.automatic_turntable"    #id name
 	bl_label = "Orbit selected object and render"        #Display Label
 	bl_options = {"REGISTER"}       #Possible operations
