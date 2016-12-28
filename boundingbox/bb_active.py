45f0eecc5fda8377904c48f2c35c928574c17ee3
ibbolia <ibbolia@gmail.com>
2016-12-27 19:07:43 -0500

    Added variables to track extreme edges

--- boundingbox/bb_active.py
+++ boundingbox/bb_active.py
@@ -14,8 +14,32 @@ print(ob.location)
 
 xPos = centre[0] + ob.location[0]
 yPos = centre[1] + ob.location[1]
-yPos = centre[2] + ob.location[2]
+zPos = centre[2] + ob.location[2]
 
 print(str(xPos) + ", " + str(yPos) + ", " + str(zPos))
+
+xMin = 0
+xMax = 0
+yMin = 0
+yMax = 0
+zMin = 0
+zMax = 0
+
 for b in ob.bound_box:
-    print(Vector(b))
+	if(Vector(b)[0] > xMax):
+		xMax = Vector(b)[0]
+	if(Vector(b)[0] < xMin):
+		xMin = Vector(b)[0]
+
+	if(Vector(b)[1] > yMax):
+		yMax = Vector(b)[1]
+	if(Vector(b)[1] < yMin):
+		yMin = Vector(b)[1]
+
+	if(Vector(b)[2] > zMax):
+		zMax = Vector(b)[2]
+	if(Vector(b)[2] < zMin):
+		zMin = Vector(b)[2]
+	print(Vector(b))
+
+print(xMax)
