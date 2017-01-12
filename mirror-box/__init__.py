5aedd5ec2c34125d9ee7c49ce792a195666724c7
ibbolia <ibbolia@gmail.com>
2017-01-11 20:08:49 -0500

    Started mirror box script

---
+++ mirror-box/__init__.py
@@ -0,0 +1,10 @@
+bl_info = {
+	"name":"Mirror Box",
+	"description":"Generate a mirrored, subdivided version of the starting cube",
+	"version":(0,1,0),
+	"blender":(2,78,0),
+	"support":"TESTING",
+	"category":"Objects",
+	"author":"Mark Fitzgibbon"
+}
+import bpy
